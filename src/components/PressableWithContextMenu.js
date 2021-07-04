9c649a4cfa5d4791acbd3db0d69b5c9cba1700bd
Andrew Ferguson <fergusonandrew4@gmail.com>
2021-07-04 09:08:27 -0500

    adding new component for links and images with context menus

---
+++ src/components/PressableWithContextMenu.js
@@ -0,0 +1,13 @@
+import { Compontent } from 'react';
+import PressableWithSecondaryInteraction from './PressableWithSecondaryInteraction';
+import ReportActionContextMenu from '../pages/home/report/ReportActionContextMenu';
+
+class PressableWithContextMenu extends Compontent {
+  printName() {
+
+  }
+
+  render() {
+
+  }
+}
